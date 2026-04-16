74a615fc765c7f215fbcefb375659de8f57e9378
SergioDawRV <slecmar974@fp.iesromerovargas.com>
2026-04-16 14:11:59 +0200

    run I

--- proyecto_final/mysite/urls.py
+++ proyecto_final/mysite/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inner/', include("inner_circle.urls")),
-    path('/accounts', include("django.contrib.auth.urls")),
+    path('accounts/', include("django.contrib.auth.urls")),
 ]
