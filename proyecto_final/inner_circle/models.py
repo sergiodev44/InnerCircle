c4e94cf01ea44f60457ef8cbe120ed25b7b005a8
SergioDawRV <slecmar974@fp.iesromerovargas.com>
2026-04-21 13:27:36 +0200

    cont vis productos y detalle perfil

--- proyecto_final/inner_circle/models.py
+++ proyecto_final/inner_circle/models.py
@@ -14,10 +14,15 @@ class Profile(models.Model):
     img_perfil = models.ImageField(upload_to="profiles/")
 
 class Product(models.Model):
+    """
+    no incluir zapatos porque no van por tallas
+    productos con medidas específicas por ejemplo correas, 
+    deberían tener un campo opcional de extra info en el form no?
+    """
     ESTADO_PRODUCTO = [("DISP","disponible"), ("RESV","reservado"), ("VEND","vendido")]
     TALLAS = [("S", "pequeña"), ("M", "mediana"), ("L", "grande"), ("XL", "muy grande")]
 
-    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
     estado = models.CharField(choices=ESTADO_PRODUCTO)
--- proyecto_final/mysite/urls.py
+++ proyecto_final/mysite/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from django.contrib.auth import views
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/inner/')),
     path('inner/', include("inner_circle.urls")),
     path('accounts/', include("django.contrib.auth.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
